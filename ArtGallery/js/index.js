420ad6a5ad807d37f35f76d3062791ae378adc12
ishmaelcarter <ishmaelcarter@email.arizona.edu>
2017-03-11 03:51:27 -0700

    Initial Commit

---
+++ ArtGallery/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function (){
+  $('#block1').click(function(){
+    $('#img1').toggle("slow");
+    $('#buy1').toggle("slow");
+  });
+
+  $('#block2').click(function(){
+    $('#img2').toggle("slow");
+    $('#buy2').toggle("slow");
+  });
+
+  $('#block3').click(function(){
+    $('#img3').toggle("slow");
+    $('#buy3').toggle("slow");
+  });
+
+  $('#block4').click(function(){
+    $('#img4').toggle("slow");
+    $('#buy4').toggle("slow");
+  });
+
+  $('#block5').click(function(){
+    $('#img5').toggle("slow");
+    $('#buy5').toggle("slow");
+  });
+});
